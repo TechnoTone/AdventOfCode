0571b084f5f5b5209e940db97fbeeb50963e2ddc
Tony Hunt <thunt@travelrepublic.co.uk>
2018-12-03 16:58:25 +0000

    more minor improvements to Day01 PowerShell solution

--- 2018/PowerShell_AoC/Day01.ps1
+++ 2018/PowerShell_AoC/Day01.ps1
@@ -1,15 +1,12 @@
 ﻿
-$data = cat (Join-Path (Join-Path ($PSCommandPath | Split-Path -Parent | Split-Path -Parent) Data) Day01.data)
+$data = cat (Join-Path (Join-Path ($PSCommandPath | Split-Path -Parent | Split-Path -Parent) Data) Day01.data) |
+        % {[int]::Parse($_)}
 
 
 
 #Part 1
 
-$total = 0
-
-$data | % {
-    $total += [int]::Parse($_)
-}
+$total = $data | measure -Sum | select -ExpandProperty Sum
 
 Write-Host "Part 1 = $total" -ForegroundColor Cyan
 
@@ -21,7 +18,7 @@ $total = 0
 $totals = @{}
 do {
     $data | % {
-        $total += [int]::Parse($_)
+        $total += $_
         if ($totals[$total]) {
             Write-Host "Part 2 = $total" -ForegroundColor Cyan
             break

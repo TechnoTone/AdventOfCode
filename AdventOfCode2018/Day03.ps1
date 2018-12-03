f83dfa8d85d2b883c65d96f8e00ee13af6a66ac3
Tony Hunt <TechnoTone@gmail.com>
2018-12-03 06:18:06 +0000

    Day 3 complete

---
+++ AdventOfCode2018/Day03.ps1
@@ -0,0 +1,41 @@
+﻿
+#Part 1
+
+$data = cat (Join-Path ($PSCommandPath | Split-Path -Parent) Day03.data)
+
+$cloth = New-Object "int[,]" 1000,1000
+
+$n=0
+foreach ($line in $data) {
+    $parts = $line.Replace(" ","").Split("@,x:") | % {try{[int]$_}catch{}}
+    for ($x=$parts[1]; $x -lt $parts[1]+$parts[3]; $x++) {
+        for ($y=$parts[2]; $y -lt $parts[2]+$parts[4]; $y++) {
+            $cloth[$x,$y]=$cloth[$x,$y]+1
+        }
+    }
+}
+
+$overlaps = ($cloth | ? {$_ -gt 1}).Count
+
+Write-Host "Part 1 = $overlaps" -ForegroundColor Cyan
+
+
+
+#Part 2
+
+foreach ($line in $data) {
+    $parts = $line.Replace(" ","").Split("@,x:") | % {try{[int]$_}catch{}}
+    $ok=$true
+    for ($x=$parts[1]; $x -lt $parts[1]+$parts[3]; $x++) {
+        for ($y=$parts[2]; $y -lt $parts[2]+$parts[4]; $y++) {
+            if ($cloth[$x,$y] -gt 1) {
+                $ok=$false
+            }
+        }
+    }
+    if ($ok) {
+        Write-Host "Part 2 = $line" -ForegroundColor Cyan
+        exit
+    }
+}
+

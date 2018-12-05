83c966cf9e2224e2373b1cabb165ea3fd004df65
Tony Hunt <TechnoTone@gmail.com>
2018-12-05 21:20:20 +0000

    minor tweats to Day 5

--- 2018/PowerShell_AoC/Day05.ps1
+++ 2018/PowerShell_AoC/Day05.ps1
@@ -1,15 +1,16 @@
-﻿
+﻿$start = Get-Date
+
 $data = cat (Join-Path (Join-Path ($PSCommandPath | Split-Path -Parent | Split-Path -Parent) Data) Day05.data)
 
-$regEx = [regex]((65..90 | %{[char]$_+[char]($_+32)+"|"+[char]($_+32)+[char]$_}) -join "|")
+$regEx = [regex]"aA|Aa|bB|Bb|cC|Cc|dD|Dd|eE|Ee|fF|Ff|gG|Gg|hH|Hh|iI|Ii|jJ|Jj|kK|Kk|lL|Ll|mM|Mm|nN|Nn|oO|Oo|pP|Pp|qQ|Qq|rR|Rr|sS|Ss|tT|Tt|uU|Uu|vV|Vv|wW|Ww|xX|Xx|yY|Yy|zZ|Zz"
+
 
 #Part 1
 while ($data -and $regEx.IsMatch($data)) {
     $data = $regEx.Replace($data, "")
 }
 
-Write-Host ("Part 1 = {0}" -f $data.Length) -ForegroundColor Cyan
-
+Write-Host ("Part 1 = {0} ({1})" -f $data.Length,(Get-Date).Subtract($start).TotalSeconds) -ForegroundColor Cyan
 
 
 #Part 2
@@ -24,6 +25,4 @@ foreach ($n in @(65..90)) {
     $results = $results + $data2.Length
 }
 
-$results
-
-Write-Host ("Part 2 = {0}" -f ($results | measure -Minimum).Minimum) -ForegroundColor Cyan
+Write-Host ("Part 2 = {0} ({1})" -f ($results | measure -Minimum).Minimum,(Get-Date).Subtract($start).TotalSeconds) -ForegroundColor Cyan

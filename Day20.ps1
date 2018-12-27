7a3aa4cafc0fdd85b42aae6fddc1b1b47f1a896d
Tony Hunt <thunt@travelrepublic.co.uk>
2018-12-27 17:13:58 +0000

    day 20 solved

--- Day20.ps1
+++ Day20.ps1
@@ -1,153 +1,111 @@
 
 function loadData ($data) {
-    $result = [PSCustomObject]@{
-        sourceData = $data
-        state = "X"
-        rooms = [System.Collections.ArrayList]::new()
-        minX = 0
-        maxX = 0
-        minY = 0
-        maxY = 0
-    } | Add-Member -PassThru -MemberType ScriptProperty -Name Width -Value {
-        $this.maxX - $this.minX + 1
-    } | Add-Member -PassThru -MemberType ScriptProperty -Name Height -Value {
-        $this.maxY - $this.minY + 1
-    } | Add-Member -PassThru -MemberType ScriptMethod -Name Show -Value {
-        $data.rooms | ft X,Y,N,E,S,W | oh
-
-    } | Add-Member -PassThru -MemberType ScriptMethod -Name RoomAt -Value {
-        param ([System.Drawing.Point]$location)
-        $r = $this.rooms | ?{$_.X -eq $location.X -and $_.Y -eq $location.Y}
-        if (!$r) { $r = $this.NewRoom($location) }
-        $r
-    } | Add-Member -PassThru -MemberType ScriptMethod -Name NewRoom -Value {
-        param ([System.Drawing.Point]$location)
-        $r = newRoom -Location $location
-        $this.rooms.Add($r) | out-null
-        $r
-    #} | Add-Member -PassThru -MemberType ScriptMethod -Name XXX -Value {        
-    } 
+    $data = $data.TrimStart("^").TrimEnd("$")
     
-    $dests = generate $result $data.TrimStart("^").TrimEnd("$")
-    Write-Host "Final Destinations:" -ForegroundColor Green
-    $dests | ft X,Y | oh
-
-    $result
-}
-
-function newRoom([int]$X,[int]$Y,[System.Drawing.Point]$Location) {
-    if ($Location) {
-        $X = $location.X
-        $Y = $location.Y
-    }
     [PSCustomObject]@{
-        X = $X
-        Y = $Y
-        N = $null
-        E = $null
-        S = $null
-        W = $null
+        sourceData = $data
+        map = walk $data
+    #} | Add-Member -PassThru -MemberType ScriptMethod -Name XXX -Value {        
     }
 }
 
-function deDup ($positions) {
-    $positions | group X,Y | % {$_.Group[0]}
-}
-
-function generate ($data,$instructions,$position=[System.Drawing.Point]::new(0,0)) {
-    Write-Host $instructions -ForegroundColor Magenta
-
-    if ($instructions.StartsWith("(")) {
-        $start = 1
-        $end = $instructions.LastIndexOf(")")-1
-        $section = $instructions.Substring($start,$end)
-        $positions = generate $data $section $position
-        $instructions = $instructions.Substring($end+2)
-    } else {
-        $end = ($instructions+"(").IndexOfAny("()")
-        $section = $instructions.Substring(0,$end)
-        if ($section.Contains("|")) {
-            $positions = deDup (
-                $section.Split("|") | % {
-                    $section = $_
-                    $positions | % { generate $data $section $_ }
-                })
+function newMap() {
+    [PSCustomObject]@{
+        rooms = @{}
+    } | Add-Member -PassThru -MemberType ScriptMethod -Name Walk -Value {
+        param ($room)
+        $key = $room.key
+        $r = $this.rooms[$key]
+        if ($r) {
+            if ($r.distance -gt $room.distance) {
+                $r.distance = $room.distance
+            } else {
+                #$room.distance = $r.distance
+            }
         } else {
-            $positions = walk $data $section $position
+            $this.rooms[$key] = $room
         }
-        $instructions = $instructions.Substring($end)
-    }
-    if ($instructions) {
-        $positions | % { generate $data $instructions $_ }
-    } else {
-        $positions
     }
 }
 
-function walk ($data,$instructions,$position) {
-    Write-Host $instructions -ForegroundColor Yellow
-    $currentRoom = $data.RoomAt($position)
-    switch ($instructions.ToCharArray()) {
-        "N" {
-                $position.Y--
-                $newRoom = $data.RoomAt($position)
-                $currentRoom.N = $newRoom
-                $newRoom.S = $currentRoom
-                $currentRoom = $newRoom
-            }
-        "E" {
-                $position.X++
-                $newRoom = $data.RoomAt($position)
-                $currentRoom.E = $newRoom
-                $newRoom.W = $currentRoom
-                $currentRoom = $newRoom
-            }
-        "S" {
-                $position.Y++
-                $newRoom = $data.RoomAt($position)
-                $currentRoom.S = $newRoom
-                $newRoom.N = $currentRoom
-                $currentRoom = $newRoom
-            }
-        "W" {
-                $position.X--
-                $newRoom = $data.RoomAt($position)
-                $currentRoom.W = $newRoom
-                $newRoom.E = $currentRoom
-                $currentRoom = $newRoom
-            }
-        default {
-                Write-Host "Oh Frell!" -ForegroundColor Yellow -BackgroundColor Red
-            }
-    }
-    return $position
+function newRoom([int]$x,[int]$y, [int]$distance, [string]$road) {
+    [PSCustomObject]@{
+        x = $x
+        y = $y
+        distance = $distance
+        #road = $road
+    } | Add-Member -PassThru -MemberType ScriptProperty -Name "key" -Value {"{0},{1}" -f $this.x,$this.y}
 }
+
+function walk ($data) {
+
+    #Write-Host "BREAKDOWN: $data" -ForegroundColor Magenta
+
+    $room = newRoom 0 0 0 ""
+    $stack = New-Object System.Collections.Stack
+    $map = newMap
+
+    foreach ($char in $data.ToCharArray()) {
+        switch ($char) {
+            "(" {
+                    $stack.Push($room)
+                }
+            ")" {
+                    $room = $stack.Pop()
+                }
+            "|" {
+                    $room = $stack.Peek()
+                }
+        }
+        if ("NESW".Contains($char)) {
+            switch ($char) {
+                "N" {$room = newRoom ($room.x) ($room.y+1) ($room.distance+1) } #($room.road+$char)}
+                "E" {$room = newRoom ($room.x+1) ($room.y) ($room.distance+1) } #($room.road+$char)}
+                "S" {$room = newRoom ($room.x) ($room.y-1) ($room.distance+1) } #($room.road+$char)}
+                "W" {$room = newRoom ($room.x-1) ($room.y) ($room.distance+1) } #($room.road+$char)}
+            }
+            if ($room.distance -ge 19) {
+                Out-Null -InputObject "agh!"
+            }
+            $map.Walk($room)
+        }
+    }
+
+    $map
+}
+
 
 #Examples
 
-cls
-$start = Get-Date
-$data = loadData "^ENWWW(NEEE|SSE(EE|N))$"
+#cls
 
-$data.Show()
+#$data = loadData "^ENWWW(NEEE|SSE(EE|N))$"
+#$data.map.rooms.Values | sort distance | select -last 1 | ft distance #should be 10
+##$data.map.rooms.Values | sort y,x | ft
 
+#$data = loadData "^ENNWSWW(NEWS|)SSSEEN(WNSE|)EE(SWEN|)NNN$"
+#$data.map.rooms.Values | sort distance | select -last 1 | ft distance #should be 18
+##$data.map.rooms.Values | sort y,x | ft
 
-Write-Host ("Part 0 = {0} ({1:0.0000})" -f $answer0,(Get-Date).Subtract($start).TotalSeconds) -ForegroundColor Cyan
+#$data = loadData "^ESSWWN(E|NNENN(EESS(WNSE|)SSS|WWWSSSSE(SW|NNNE)))$"
+#$data.map.rooms.Values | sort distance | select -last 1 | ft distance #should be 23
+##$data.map.rooms.Values | sort y,x | ft
 
-return
+#$data = loadData "^WSSEESWWWNW(S|NENNEEEENN(ESSSSW(NWSW|SSEN)|WSWWN(E|WWS(E|SS))))$"
+#$data.map.rooms.Values | sort distance | select -last 1 | ft distance #should be 31
+##$data.map.rooms.Values | sort y,x | ft
+
+#return
 
 #Part 1
 
 $start = Get-Date
-
+$data = loadData (cat (Join-Path ($PSCommandPath | Split-Path -Parent) Day20.data))
+$answer1 = $data.map.rooms.Values | sort distance | select -last 1 | select -ExpandProperty distance
 Write-Host ("Part 1 = {0} ({1:0.0000})" -f $answer1,(Get-Date).Subtract($start).TotalSeconds) -ForegroundColor Cyan
-
-
 
 #Part 2
 
 $start = Get-Date
-
+$answer2 = $data.map.rooms.Values | ? {$_.distance -ge 1000} | measure | select -ExpandProperty Count
 Write-Host ("Part 2 = {0} ({1:0.0000})" -f $answer2,(Get-Date).Subtract($start).TotalSeconds) -ForegroundColor Cyan
-
-

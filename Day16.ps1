01d9889dea8eaabae72bbdd9b440e2aae23928bf
Tony Hunt <thunt@travelrepublic.co.uk>
2019-03-21 14:54:59 +0000

    Day 16 part 1 complete

--- Day16.ps1
+++ Day16.ps1
@@ -12,20 +12,27 @@ function parseData($data) {
                 $result.Before = $v
             } else {
                 $result.After = $v
-                Write-Output $result
+                $result
                 $result = [PSCustomObject] @{Before=$null;Inst=$null;After=$null}
             }
+        }
+    } | Add-Member -PassThru -MemberType ScriptMethod -Force -Name "ToString" -Value {
+        [PSCustomObject] @{
+            Before = $this.Before -join ","
+            Inst = $this.Inst -join ","
+            After = $this.After -join ","
         }
     }
 }
 
 
 $device = [PSCustomObject]@{
-    R=@(0,0,0,0)
+    R = @(0,0,0,0)
+    I = @()
 } | Add-Member -PassThru -MemberType ScriptMethod -Name "Init" -Value {
     param($r1,$r2,$r3,$r4)
     if ($r1 -is [Array]) {
-        $this.R = $r1
+        $this.R = $r1.Clone()
     } else {
         $this.R = @($r1,$r2,$r3,$r4)
     }
@@ -36,25 +43,117 @@ $device = [PSCustomObject]@{
     } else {
         ($this.R -join ",") -eq (@($r1,$r2,$r3,$r4) -join ",")
     }
+} | Add-Member -PassThru -MemberType ScriptMethod -Name "addf" -Value {
+    #addr (add register) stores into register C the result of adding register A and register B.
+    param ($A,$B,$C)
+    $this.R[$C] = $this.R[$A] + $this.R[$B]
+} | Add-Member -PassThru -MemberType ScriptMethod -Name "addi" -Value {
+    #addi (add immediate) stores into register C the result of adding register A and value B.
+    param ($A,$B,$C)
+    $this.R[$C] = $this.R[$A] + $B
+} | Add-Member -PassThru -MemberType ScriptMethod -Name "mulr" -Value {
+    #mulr (multiply register) stores into register C the result of multiplying register A and register B.
+    param ($A,$B,$C)
+    $this.R[$C] = $this.R[$A] * $this.R[$B]
+} | Add-Member -PassThru -MemberType ScriptMethod -Name "muli" -Value {
+    #muli (multiply immediate) stores into register C the result of multiplying register A and value B.
+    param ($A,$B,$C)
+    $this.R[$C] = $this.R[$A] * $B
+} | Add-Member -PassThru -MemberType ScriptMethod -Name "banr" -Value {
+    #banr (bitwise AND register) stores into register C the result of the bitwise AND of register A and register B.
+    param ($A,$B,$C)
+    $this.R[$C] = $this.R[$A] -band $this.R[$B]
+} | Add-Member -PassThru -MemberType ScriptMethod -Name "bani" -Value {
+    #bani (bitwise AND immediate) stores into register C the result of the bitwise AND of register A and value B.
+    param ($A,$B,$C)
+    $this.R[$C] = $this.R[$A] -band $B
+} | Add-Member -PassThru -MemberType ScriptMethod -Name "borr" -Value {
+    #borr (bitwise OR register) stores into register C the result of the bitwise OR of register A and register B.
+    param ($A,$B,$C)
+    $this.R[$C] = $this.R[$A] -bor $this.R[$B]
+} | Add-Member -PassThru -MemberType ScriptMethod -Name "bori" -Value {
+    #bori (bitwise OR immediate) stores into register C the result of the bitwise OR of register A and value B.
+    param ($A,$B,$C)
+    $this.R[$C] = $this.R[$A] -bor $B
+} | Add-Member -PassThru -MemberType ScriptMethod -Name "setr" -Value {
+    #setr (set register) copies the contents of register A into register C. (Input B is ignored.)
+    param ($A,$B,$C)
+    $this.R[$C] = $this.R[$A]
+} | Add-Member -PassThru -MemberType ScriptMethod -Name "seti" -Value {
+    #seti (set immediate) stores value A into register C. (Input B is ignored.)
+    param ($A,$B,$C)
+    $this.R[$C] = $A
+} | Add-Member -PassThru -MemberType ScriptMethod -Name "gtir" -Value {
+    #gtir (greater-than immediate/register) sets register C to 1 if value A is greater than register B. Otherwise, register C is set to 0.
+    param ($A,$B,$C)
+    $this.R[$C] = [int]($A -gt $this.R[$B])
+} | Add-Member -PassThru -MemberType ScriptMethod -Name "gtri" -Value {
+    #gtri (greater-than register/immediate) sets register C to 1 if register A is greater than value B. Otherwise, register C is set to 0.
+    param ($A,$B,$C)
+    $this.R[$C] = [int]($this.R[$A] -gt $B)
+} | Add-Member -PassThru -MemberType ScriptMethod -Name "gtrr" -Value {
+    #gtrr (greater-than register/register) sets register C to 1 if register A is greater than register B. Otherwise, register C is set to 0.
+    param ($A,$B,$C)
+    $this.R[$C] = [int]($this.R[$A] -gt $this.R[$B])
+} | Add-Member -PassThru -MemberType ScriptMethod -Name "eqir" -Value {
+    #eqir (equal immediate/register) sets register C to 1 if value A is equal to register B. Otherwise, register C is set to 0.
+    param ($A,$B,$C)
+    $this.R[$C] = [int]($A -eq $this.R[$B])
+} | Add-Member -PassThru -MemberType ScriptMethod -Name "eqri" -Value {
+    #eqri (equal register/immediate) sets register C to 1 if register A is equal to value B. Otherwise, register C is set to 0.
+    param ($A,$B,$C)
+    $this.R[$C] = [int]($this.R[$A] -eq $B)
+} | Add-Member -PassThru -MemberType ScriptMethod -Name "eqrr" -Value {
+    #eqrr (equal register/register) sets register C to 1 if register A is equal to register B. Otherwise, register C is set to 0.
+    param ($A,$B,$C)
+    $this.R[$C] = [int]($this.R[$A] -eq $this.R[$B])
 }
 
+$device.I = $device | gm -MemberType ScriptMethod | select -ExpandProperty Name | ? {$_ -notmatch "Equals|Init"}
 
 
+function testDevice ($test) {
+    $A = $test.Inst[1]
+    $B = $test.Inst[2]
+    $C = $test.Inst[3]
+    
+    #Write-Host $test
+
+    foreach ($I in $device.I) {
+        #Write-Host $test.Before -ForegroundColor Gray
+        $device.Init($test.Before)
+        $device.$I($A,$B,$C)
+        if ($device.Equals($test.After)) {
+            $I
+            #Write-Host "$I "($device.R -join ",") -ForegroundColor Green
+        #} else {
+           # Write-Host "$I "($device.R -join ",") -ForegroundColor Red
+        }
+    }
+#    pause
+}
 
 #Examples
 
-$data = parseData ( cat (Join-Path ($PSCommandPath | Split-Path -Parent) .\day16.examples.txt) )
+cls
 
-$data
-
-
-
-
-return
 
 #Part 1
 
 $start = Get-Date
+
+$answer1 = 0
+
+$data = parseData ( cat (Join-Path ($PSCommandPath | Split-Path -Parent) .\day16.examples.txt) )
+$data | % {
+
+    $instructions = testDevice $_
+    #Write-Host $_ ($instructions -join ",")
+    if ($instructions.Count -ge 3) {
+        $answer1++
+    }
+
+}
 
 Write-Host ("Part 1 = {0} ({1:0.0000})" -f $answer1,(Get-Date).Subtract($start).TotalSeconds) -ForegroundColor Cyan
 

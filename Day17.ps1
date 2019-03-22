3aafbf3e0408e9238bf99a96e6de1cb30cea429d
Tony Hunt <thunt@travelrepublic.co.uk>
2019-03-22 16:56:27 +0000

    Day17 WIP...

--- Day17.ps1
+++ Day17.ps1
@@ -1,9 +1,50 @@
 
 
-$data = cat (Join-Path ($PSCommandPath | Split-Path -Parent) Day17.data)
+
+function parseData($data) {
+
+    #find minimum/maximum x and y
+    #create grid array of appropriate size
+    #load grid with $data
+    
+    $grid = New-Object "int[,]" $dX,$dY
+    
+    $result = [PSCustomObject] @{Grid}
+    $data | % {
+        $l = $_ -split ":"
+        if ($l.Count -eq 1) {
+            $result.Inst = $l -split " " | % {[int]$_}
+        } else {
+            $v = $l[1] -replace "[\[|\]| ]","" -split "," | % {[int]$_}
+            if ($l[0] -eq "Before") {
+                $result.Before = $v
+            } else {
+                $result.After = $v
+                $result
+                $result = [PSCustomObject] @{Before=$null;Inst=$null;After=$null}
+            }
+        }
+    } | Add-Member -PassThru -MemberType ScriptMethod -Force -Name "ToString" -Value {
+        [PSCustomObject] @{
+            Before = $this.Before -join ","
+            Inst = $this.Inst -join ","
+            After = $this.After -join ","
+        }
+    }
+}
 
 
 
+
+
+
+$data = parseData ( cat (Join-Path ($PSCommandPath | Split-Path -Parent) Day17.test) )
+
+
+
+$data
+
+return
 #Examples
 
 

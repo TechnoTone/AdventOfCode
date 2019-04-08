87872fdb0d5b1a31d49f1ef12b0fbdfb027503a0
Tony Hunt <thunt@travelrepublic.co.uk>
2019-04-08 16:59:09 +0100

    Day17 WIP...

--- Day17.ps1
+++ Day17.ps1
@@ -1,45 +1,76 @@
 
+$cellEmpty = '.'
+$cellClay = '#'
+$cellSpring = '+'
+$cellWaterFalling = '|'
+$cellWaterFlat = '~'
 
 
-function parseData($data) {
-
-    #find minimum/maximum x and y
-    #create grid array of appropriate size
-    #load grid with $data
-    
-    $grid = New-Object "int[,]" $dX,$dY
-    
-    $result = [PSCustomObject] @{Grid}
-    $data | % {
-        $l = $_ -split ":"
-        if ($l.Count -eq 1) {
-            $result.Inst = $l -split " " | % {[int]$_}
-        } else {
-            $v = $l[1] -replace "[\[|\]| ]","" -split "," | % {[int]$_}
-            if ($l[0] -eq "Before") {
-                $result.Before = $v
+function parseCoordinates($lines) {
+    foreach ($line in $lines) {
+        $values = ($line -split ", ") | % {($_ -split "=")[1]}
+        $range = $values[1] -split "\.\."
+        @($range[0]..$range[1]) | % {
+            if ($line.StartsWith("x")) {
+                [PSCustomObject]@{X=$values[0];Y=$_}
             } else {
-                $result.After = $v
-                $result
-                $result = [PSCustomObject] @{Before=$null;Inst=$null;After=$null}
+                [PSCustomObject]@{X=$_;Y=$values[0]}
             }
-        }
-    } | Add-Member -PassThru -MemberType ScriptMethod -Force -Name "ToString" -Value {
-        [PSCustomObject] @{
-            Before = $this.Before -join ","
-            Inst = $this.Inst -join ","
-            After = $this.After -join ","
         }
     }
 }
 
+function parseData($lines) {
 
+    $coords = parseCoordinates $lines
+    $xMin = ($coords.X | Measure-Object -Minimum).Minimum
+    $xMax = ($coords.X | Measure-Object -Maximum).Maximum
+    $yMin = ($coords.Y | Measure-Object -Minimum).Minimum
+    $yMax = ($coords.Y | Measure-Object -Maximum).Maximum
 
+    if ($yMin -gt 0) {$yMin = 0}
 
+    $dx = $xMax - $xMin
+    $dy = $yMax - $yMin
+
+    $grid = @()
+
+    @(0..$dy) | % {
+        $grid = $grid + [string]::new($cellEmpty,$dx+1)
+    }
+
+    $data = [PSCustomObject]@{
+        grid=$grid
+        xMin=$xMin
+        xMax=$xMax
+        yMin=$yMin
+        yMax=$yMax
+        width=$dx
+        height=$dy
+    } | Add-Member -PassThru -MemberType ScriptMethod -Name "getCell" -Value {
+        param ($x,$y)
+        $x = $x-$this.xmin
+        $y = $y-$this.yMin
+        $this.grid[$y][$x]
+    } | Add-Member -PassThru -MemberType ScriptMethod -Name "setCell" -Value {
+        param ($x,$y,$value)
+        $x = $x-($this.xMin)
+        $y = $y-($this.yMin)
+        $this.grid[$y] = ($this.grid[$y]).Remove($x,1).Insert($x,$value)
+    } | Add-Member -PassThru -MemberType ScriptMethod -Force -Name "displayGrid" -Value {
+        $this.grid
+    }
+
+    $data.setCell(500, 0, $cellSpring)
+    
+    $coords | % { $data.setCell( $_.X, $_.Y, $cellClay ) }
+
+    return $data
+        
+}
 
 
 $data = parseData ( cat (Join-Path ($PSCommandPath | Split-Path -Parent) Day17.test) )
-
 
 
 $data

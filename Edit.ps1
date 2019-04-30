453aa2bf60c2e36019c1fad1e77427d4a756dd56
TechnoTone <TechnoTone@gmail.com>
2019-04-30 11:48:07 +0100

    Updated tools

--- Edit.ps1
+++ Edit.ps1
@@ -1,5 +1,6 @@
 ﻿git log -10 --format=format:'%s' | 
-    grep 'Day\d+' | 
+    grep 'Day\s*\d+' | 
     select -First 1 * -ExpandProperty Matches | 
     select -ExpandProperty Value |
+    % {$_ -replace " ",""} |
     % {psedit "$_.ps1"}
---
+++ Run.ps1
@@ -0,0 +1,6 @@
+﻿git log -10 --format=format:'%s' | 
+    grep 'Day\s*\d+' | 
+    select -First 1 * -ExpandProperty Matches | 
+    select -ExpandProperty Value |
+    % {$_ -replace " ",""} |
+    % {cls; . ".\$_.ps1"}
